a930e73ae03d94be774a0e9c395106d5325aa6ab
dalmariko <dalmarikocamp@gmail.com>
2019-05-30 04:20:35 +0300

    donate field done

--- src/components/donations/DonationsInfo.vue
+++ src/components/donations/DonationsInfo.vue
@@ -1,25 +1,25 @@
 <template>
-<div class="donationsInfo" >
+  <div class="donationsInfo">
 
-  <div class="fild">
+    <div class="fild">
       <p class="head">rased</p>
-    <p class="rased">{{donations.rased}}</p>
+      <p class="rased">{{donations.rased}}</p>
     </div>
 
-  <div class="fild">
-    <p class="head">donors</p>
-    <p class="donors">{{donations.donors}}</p>
-  </div>
+    <div class="fild">
+      <p class="head">donors</p>
+      <p class="donors">{{donations.donors}}</p>
+    </div>
 
-  <div class="fild">
-    <p class="head">date</p>
-    <p class="date">{{donations.date}}</p>
-  </div>
+    <div class="fild">
+      <p class="head">date</p>
+      <p class="date">{{donations.date}}</p>
+    </div>
 
-  <a href="#" class="donateButton">
-See the campaign
-  </a>
-</div>
+    <a href="#" class="donateButton">
+      See the campaign
+    </a>
+  </div>
 </template>
 
 <script>
@@ -31,60 +31,66 @@ export default {
 
 <style scoped lang="scss">
 
-  .donationsInfo{
-  width: 650px;
-  height: 70px;
-  background-color: #ffffff;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  .donationsInfo {
+    width: 650px;
+    height: 70px;
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
     align-items: center;
-  margin: 1.7rem 0;
-}
+    margin: 1.7rem 0;
+  }
+
   .fild {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
-  }
-  .head{
-    font-family: 'VitoLight';
-    font-size: 1rem;
-    text-transform: uppercase;
-    color:#acadb6;
-    margin-bottom: 1rem ;
+    align-items: flex-start;
   }
 
-  %fildfooter{
-    font-family: 'OpenSansSemiBold';
+  .head {
+    font-family: 'VitoLight', serif;
+    font-size: 1rem;
+    text-transform: uppercase;
+    color: #acadb6;
+    margin-bottom: .8rem;
+  }
+
+  %fildfooter {
+    font-family: 'OpenSansSemiBold', serif;
     font-size: 1.6rem;
   }
-  .rased{
+
+  .rased {
     @extend %fildfooter;
     color: #107598;
   }
-  .donors{
+
+  .donors {
     @extend %fildfooter;
-    color:#00d39a;
+    color: #00d39a;
   }
-  .date{
-   @extend %fildfooter;
+
+  .date {
+    @extend %fildfooter;
     color: #ffb968;
   }
 
-  .donateButton{
+  .donateButton {
     display: flex;
-    height: 36px;
-    min-width: 160px;
+    height: 3.6rem;
+    min-width: 16rem;
     align-items: center;
     justify-content: center;
-    border-radius: 18px;
-    background-color:#ff5555;
+    border-radius: 1.8rem;
+    background-color: #ff5555;
     flex-shrink: 0;
-    font-family: 'OpenSansBold',serif;
+    font-family: 'OpenSansBold', serif;
     font-size: 1.4rem;
     color: #fff;
-    &:hover{
+
+    &:hover {
       background-color: #f26e66;
     }
   }
